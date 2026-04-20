81ba6f5bab9a3c4818e3affb579d325dd241cae7
Lbaez12 <lbaez@fi.uba.ar>
2026-04-20 17:51:06 -0300

    get_partido_by_id a service
    se agrega get_partido_by_id a service

--- src/partidos/service.py
+++ src/partidos/service.py
@@ -13,14 +13,20 @@ def crear_partido(data):
         estadio=data.get("estadio"),
         ciudad=data.get("ciudad"),
         fecha=data["fecha"],
-        fase=data["fase"]
+        fase=data["fase"],
     )
 
     return nuevo_id
 
 
 def get_partidos(equipo=None, fecha=None, fase=None, limit=10, offset=0):
-    return repository.get_partidos(equipo=equipo, fecha=fecha, fase=fase, limit=limit, offset=offset)
+    return repository.get_partidos(
+        equipo=equipo, fecha=fecha, fase=fase, limit=limit, offset=offset
+    )
+
+
+def get_partido_by_id(partido_id):
+    return repository.get_partido_by_id(partido_id)
 
 
 def actualizar_resultado(partido_id, data):
@@ -39,7 +45,9 @@ def actualizar_resultado(partido_id, data):
     if goles_local < 0 or goles_visitante < 0:
         raise ValueError("Los goles no pueden ser negativos")
 
-    actualizar_resultado = repository.actualizar_resultado(partido_id, goles_local, goles_visitante)
+    actualizar_resultado = repository.actualizar_resultado(
+        partido_id, goles_local, goles_visitante
+    )
     if not actualizar_resultado:
         raise LookupError("Partido no encontrado")
 
@@ -48,6 +56,7 @@ def eliminar_partido(partido_id):
     eliminado = repository.eliminar_partido(partido_id)
     if not eliminado:
         raise ValueError("Partido no encontrado")
+
 
 def reemplazar_partido(partido_id, data):
     campos_requeridos = ["equipo_local", "equipo_visitante", "fecha", "fase"]
@@ -62,7 +71,7 @@ def reemplazar_partido(partido_id, data):
         estadio=data.get("estadio"),
         ciudad=data.get("ciudad"),
         fecha=data["fecha"],
-        fase=data["fase"]
+        fase=data["fase"],
     )
     if not exito:
         raise LookupError("Partido no encontrado")
@@ -73,10 +82,13 @@ def actualizar_partido_parcial(id_partido, datos):
     if exito:
         return {"mensaje": "Partido actualizado con éxito"}, 200
     else:
-        return {"error": "No se pudo actualizar. El partido no existe o hubo un problema en la base de datos"}, 404
+        return {
+            "error": "No se pudo actualizar. El partido no existe o hubo un problema en la base de datos"
+        }, 404
+
 
 def predecir_partido(id_partido, datos):
-    
+
     goles_local = datos["local"]
     goles_visitante = datos["visitante"]
     id_usuario = datos["id_usuario"]
@@ -87,11 +99,13 @@ def predecir_partido(id_partido, datos):
         raise ValueError("Los goles no pueden ser negativos")
     if not isinstance(id_usuario, int):
         raise ValueError("La id de usuario es un entero positivo")
-    
-    prediccion = repository.guardar_prediccion(id_partido, id_usuario, goles_local, goles_visitante)
+
+    prediccion = repository.guardar_prediccion(
+        id_partido, id_usuario, goles_local, goles_visitante
+    )
     if prediccion == 1:
         raise LookupError("Partido inexistente")
     elif prediccion == 2:
         raise LookupError("Usuario inexistente")
-    
-    return True 
+
+    return True
